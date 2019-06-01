aa64fd87e6ca77bdd070783644e0abe70e36abac
unknown <does@not.matter>
2019-06-01 08:24:07 +0200

    update build script

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ mkdir -p build/images/inventoryimages
 mkdir -p build/anim
 
 echo "compiling animations..."
-"C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools\mod_tools\scml.exe" compostpile.scml .
-"C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools\mod_tools\scml.exe" ui_compostpile1c6x3/ui_compostpile1c6x3.scml .
+# "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools\mod_tools\scml.exe" compostpile.scml .
+# "C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Mod Tools\mod_tools\scml.exe" ui_compostpile1c6x3/ui_compostpile1c6x3.scml .
 
 echo "copying files and scripts..."
 cp -r scripts/* build/scripts/
@@ -18,7 +18,7 @@ cp images/inventoryimages/*.xml build/images/inventoryimages
 cp images/inventoryimages/*.tex build/images/inventoryimages
 cp -r anim/*.zip build/anim/
 cp CONTRIBUTORS build/
-cp compostingtogether.* build/
+cp rabbitwheel.* build/
 cp LICENSE build/
 cp *.lua build/
 cp README* build/
